a49fa4c076cba65e4466405868c5db47621b0a4b
Alok Singh <aloksingh36757@gmail.com>
2024-09-09 23:10:54 +0530

    teacher and student api data set successfully

--- src/pages/api/teacherStudent.ts
+++ src/pages/api/teacherStudent.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 interface Person {
   name: string;
+  subject:string;
 }
 
 let students: Person[] = [];
@@ -17,17 +18,17 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       break;
 
     case 'POST': {
-      const { role, name } = req.body as { role: string; name: string };
-      if (!name || !role) {
-        res.status(400).json({ error: 'Missing role or name in request body' });
+      const { role, name,subject } = req.body as { role: string; name: string;subject: string };
+      if (!name || !role || !subject) {
+        res.status(400).json({ error: 'Missing role or name or subject in request body' });
         return;
       }
 
       if (role === 'teacher') {
-        teachers.push({ name });
+        teachers.push({ name,subject });
         res.status(201).json({ message: 'Teacher added successfully' });
       } else if (role === 'student') {
-        students.push({ name });
+        students.push({ name,subject });
         res.status(201).json({ message: 'Student added successfully' });
       } else {
         res.status(400).json({ error: 'Invalid role' });
@@ -47,9 +48,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       }
 
       if (deleteRole === 'teacher') {
+          // Remove teacher by name
         teachers = teachers.filter((teacher) => teacher.name !== deleteName);
         res.status(200).json({ message: 'Teacher removed' });
       } else if (deleteRole === 'student') {
+        // Remove student by name
         students = students.filter((student) => student.name !== deleteName);
         res.status(200).json({ message: 'Student removed' });
       } else {

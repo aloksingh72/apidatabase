4cbe82e83aa4d1e15557c57b9c310e3d2b910fb7
Alok Singh <aloksingh36757@gmail.com>
2024-09-13 00:32:03 +0530

    ui design started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,36 @@
 
-import { FC } from "react"; // Import FC type for defining a functional component type.
+import { FC } from "react"; 
 
 const Home: FC = () => {
   return (
-    <div className="background flex items-center justify-center h-screen">
-         <a href="http://localhost:3000/api/teacherStudent" target="_blank" className="no-underline">
-    <button className="bg-gradient-to-r from-blue-500 to-green-500 text-white font-semibold py-4 px-8 rounded-full shadow-xl transition duration-300 ease-in-out transform hover:scale-110 hover:rotate-2 hover:from-green-500 hover:to-blue-500  hover:animate-spin-slow">
+    <div className="bg-black w-full h-full">
 
-        Click for API Data
-     
-    </button>
-    </a>
+
+      <h2>Welcome to the api database</h2>
+      <div></div>
+
+
+
+
+
+
+
+
+
+
+
+
+      
   </div>
   );
 }
 
 export default Home;
 
+   {/* <a href="http://localhost:3000/api/teacherStudent" target="_blank" className="no-underline">
+    <button className="bg-gradient-to-r from-blue-500 to-green-500 text-white font-semibold py-4 px-8 rounded-full shadow-xl transition duration-300 ease-in-out transform hover:scale-110 hover:rotate-2 hover:from-green-500 hover:to-blue-500  hover:animate-spin-slow">
+
+        Click for API Data
+     
+    </button>
+    </a> */}

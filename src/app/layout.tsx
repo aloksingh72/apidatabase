165617cdee058f9e69b576d33f3beecd2e76a3f1
Alok Singh <112793361+aloksingh72@users.noreply.github.com>
2025-05-19 23:53:25 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+// geist mini local font
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
